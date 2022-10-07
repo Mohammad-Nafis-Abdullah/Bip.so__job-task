c447b88f1672bba1283306fa4b1f1dbb4e316c42
Muhammad-Nafis-Abdullah <nafisabdullah2@gmail.com>
2022-10-07 16:45:11 +0600

    movie card created

---
+++ src/components/home/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const Card = ({movie}) => {
+    return (
+        <div className='basis-44 grow-0 shrink rounded-md cursor-pointer'>
+
+            <img src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`} alt="" className='h-[17rem] w-full rounded' />
+
+            <h3 className='text-white font-medium whitespace-nowrap w-40 text-ellipsis overflow-hidden'>{movie?.title}</h3>
+
+            <small className='flex items-center gap-2 text-yellow-500 font-bold'>
+                <FaStar />
+                {movie?.vote_average}
+            </small>
+
+        </div>
+    );
+};
+
+export default Card;
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
+import useRefetch from '../../hooks/useRefetch';
+import Loading from '../preBuild/Loading';
+import Card from './Card';
 import Pagination from './Pagination';
 
 const Home = () => {
     const [page,setPage] = useState(1);
 
-    
+    const [data,loading] = useRefetch(`https://movie-task.vercel.app/api/popular?page=${page}`,[]);
+    // console.log(data?.data?.results);
 
     return (
-        <div className='min-h-screen py-10'>
-            
+        <div className='min-h-screen p-10'>
+            { loading && <Loading/>}
+
+            <section className='flex justify-center items-center flex-wrap mb-10 gap-5 max-w-[65rem] mx-auto'>
+                {
+                    data?.data?.results?.map((movie)=>
+                    <Card key={movie.id} movie={movie}/>)
+                }
+            </section>
 
             <section className='flex justify-center items-center'>
                 <Pagination page={page} setPage={setPage} />
